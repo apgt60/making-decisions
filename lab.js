d152106fabc753af9ca85c826f41640718bca18d
Amit <apgt60@github.com>
2023-11-08 17:13:35 -0500

    Amit was here

--- lab.js
+++ lab.js
@@ -1,4 +1,5 @@
 ////////// PROBLEM 1 //////////
+Amit was here
 /*
   Create a variable called 'lovesCode' and set it equal to true. 
   Check to see if 'lovesCode' is equal to true or false. 
@@ -36,8 +37,13 @@ let temperature = 55;
 let rain = true;
 
 /*
-  Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, based on the above temperature and rain.
-  If it is 80 degrees or above, and raining, one should wear a t-shirt and take an umbrella. If it is between 60 and 80 degrees, and raining, one should wear a rain-jacket. If it is 60 degrees or below, one should wear a jacket and carry an umbrella. Come up with your own clothing suggestions based on those temperature parameters when it is not raining.
+  Using an if statement and a comparison operator, create code that will log 
+  a suggestion on what type of clothes one should wear for the day, based on the above temperature and rain.
+  If it is 80 degrees or above, and raining, one should wear a t-shirt and 
+  take an umbrella. If it is between 60 and 80 degrees, and raining, one 
+  should wear a rain-jacket. If it is 60 degrees or below, one should wear 
+  a jacket and carry an umbrella. Come up with your own clothing suggestions 
+  based on those temperature parameters when it is not raining.
 */
 
 //CODE HERE
@@ -73,7 +79,10 @@ let score = 0;
 let passingScore = 7;
 
 /*
-  Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
+  Create a while-loop that logs "Your score is not high enough" while the 
+  score variable above is below passingScore. In each iteration of the loop, 
+  increase the score by one. If your code is successful, you will see your 
+  logged statement 7 times.
 */
 
 //CODE HERE
@@ -99,6 +108,11 @@ let passingScore = 7;
 ////////// PROBLEM 11 //////////
 
 let z = 5;
-// Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
+// Create a while loop that continues to run while z is greater than 0. 
+//Within the while loop, log a countdown from the value of z to 1. 
+//Once you have logged the countdown, make sure you decrement z by 1. 
+//If your code is running properly, 
+//you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
 //CODE HERE
+
